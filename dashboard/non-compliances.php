6720b6b37ebe71c813986721b98e1223a9dd4763
Klay4 <kevin.azemi@protonmail.ch>
2022-06-03 10:36:41 +0200

    Added page on GET
    a

--- dashboard/non-compliances.php
+++ dashboard/non-compliances.php
@@ -202,7 +202,7 @@
                                             $search = $_GET['search'];
                                         }
 
-                                        $nonCompliances = getNonCompliances(10, 1, $search);
+                                        $nonCompliances = getNonCompliances(10, $_GET['page'], $search);
                                         $nonCompliancesType = getNonComplianceTypes();
 
                                         foreach($nonCompliances["result"] as $ncList) {
@@ -255,17 +255,12 @@
                             <p class="m-0 text-muted">Showing <span>1</span> to <span>8</span> of <span>16</span> entries</p>
                                 <ul class="pagination m-0 ms-auto">
                                 <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><desc>Download more icon variants from https://tabler-icons.io/i/chevron-left</desc><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="15 6 9 12 15 18"></polyline></svg>
-                                    prev
+                                    <a class="page-link" href="./non-compliances.php?page=1" tabindex="-1" aria-disabled="true">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><desc>Download more icon variants from https://tabler-icons.io/i/chevron-left</desc><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="15 6 9 12 15 18"></polyline></svg>
+                                        prev
                                     </a>
                                 </li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                                <li class="page-item"><a class="page-link" href="#">5</a></li>
                                 <li class="page-item">
                                     <a class="page-link" href="#">
                                     next <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
